d8b24ff7c2495a37a85de081a58f7d5622eee08d
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-12-29 18:41:25 +0100

    First steps: read_u32

--- css_2324/src/main.rs
+++ css_2324/src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter the first number and press enter.");
+    let a = read_u32();
+    println!("Please enter the second number and press enter.");
+    let b = read_u32();
+
+    extended_euclidian_algorithm(a, b);
+}
+
+fn read_u32() -> u32 {
+    let mut buffer = String::new();
+    let mut input;
+    let mut val;
+    while let Err(_) = {
+        // get input
+        buffer.clear();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Unable to read from stdin!");
+        input = buffer.trim();
+        // parse input, evaluate block to val for the wile let loop
+        val = input.parse::<u32>();
+        &val
+    } {
+        println!("Invalid input: <{input}>, please try again.");
+    }
+    // invariant: val is now a valid u32
+    return val.expect("How did we get here?");
+}
+
+fn extended_euclidian_algorithm(a: u32, b: u32) {
+    // testing implementation
+    println!("a: {a}, b: {b}");
 }
