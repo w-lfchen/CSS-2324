bf457de125d3ea71e26551711d4667e741948da3
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-12-29 16:55:37 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "CSS-2324 Bonus exercise flake";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustfmt
+          rustc
+          cargo
+          sccache
+          rust-analyzer
+          clippy
+        ];
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        shellHook = ''
+          export RUSTC_WRAPPER=sccache
+        '';
+      };
+    };
+}
